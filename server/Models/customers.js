6d30a7ac66427bbd081ef5e1b567c3de1abcaff3
luyunhai <luyunhai@58ganji.com>
2017-08-07 22:38:15 +0800

    修改promise

--- server/Models/customers.js
+++ server/Models/customers.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose'
 import db from './db'
+import Q from 'q';
+
 mongoose.Promise = global.Promise
 const UserSchema = new mongoose.Schema({
     name: String,
@@ -7,11 +9,17 @@ const UserSchema = new mongoose.Schema({
     id: String,
     company: String
 });
-UserSchema.methods.getCustomers = function(query,cb){
+UserSchema.methods.getCustomers = function(query, cb){
     if(query === null){
         return this.model('customers').find(cb);
     }
     this.model('customers').findOne(query,cb);
+    // const deferred = Q.defer();
+    // if(query === null){
+    //     return this.model('customers').find(deferred.resolve);
+    // }
+    // this.model('customers').findOne(query, deferred.resolve);
+    // return deferred.promise;
 };
 
 export default db.model('customers',UserSchema);
--- server/api/customers.js
+++ server/api/customers.js
@@ -1,6 +1,19 @@
 // const jwt = require("jwt-simple");
 import qs from 'qs';
+import Q from 'q';
 import Customers from '../Models/customers'
+
+const ext = (customersEntity, customer) => {
+    const deferred = Q.defer();
+    customersEntity.getCustomers(customer, (err,user) => {
+        if(err){
+            deferred.reject(err);
+        } else {
+            deferred.resolve(user);
+        }
+    });
+    return deferred.promise;
+};
 
 export default function(req,res,next){
     const customer = {
@@ -8,18 +21,25 @@ export default function(req,res,next){
     };
     const customersEntity = new Customers();
 
-    customersEntity.getCustomers(
-        customer
-    ,(err,user)=>{
-        if(user){
-            return res.status(200).json({
-                name: user.name,
-                email: user.email,
-                id: user.id,
-                company: user.company
-            })
-        } else {
-            return res.status(404).end()
-        }
-    })
+    // customersEntity.getCustomers(customer, (err,user)=>{
+    //     if(user){
+    //         return res.status(200).json({
+    //             name: user.name,
+    //             email: user.email,
+    //             id: user.id,
+    //             company: user.company
+    //         })
+    //     } else {
+    //         return res.status(404).end()
+    //     }
+    // })
+
+    ext(customersEntity, customer).then(user => {
+        return res.status(200).json({
+            name: user.name,
+            email: user.email,
+            id: user.id,
+            company: user.company
+        });
+    }, err => res.status(404).end());
 }
