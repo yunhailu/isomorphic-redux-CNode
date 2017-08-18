eded78d4e2e17431acb7db70cd9decb042744667
lishoulong <2716648627@qq.com>
2017-08-18 18:41:39 +0800

    config

--- config.js
+++ config.js
@@ -1,17 +1,6 @@
 const baseDir = __dirname
 
-// export default {
-//     dbaddr:'localhost',
-//     dbport: 27017,
-//     db: 'fred-dev',
-//     dbuser: '',
-//     dbpwd: '',
-//     serverip: 'localhost',
-//     serverport: 3000,
-//     cookieDomain: '.58.com',
-//     baseDir
-// }
-module.exports = {
+export default {
     dbaddr:'localhost',
     dbport: 27017,
     db: 'fred-dev',
